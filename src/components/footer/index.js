3c0ee38e8e8adca84e6f44010fe69ebfaaaa2901
“yanghaiyang <l>
2020-05-05 20:49:36 +0800

    111

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Button } from 'antd'
 import './index.less'
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className="footer">
-               版权所有：https://github.com/Jacky-Summer
-            </div>
-        );
-    }
+  render() {
+    return <div className="footer">版权所有</div>
+  }
 }
 
-export default Footer;
+export default Footer
